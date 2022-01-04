fc72ca8d95119f6c2e036e95e97f05b0c8dfea76
Michał Garapich <michal@garapich.pl>
2022-01-04 02:41:21 +0100

    feat(tf2-base): parse status command

--- packages/tf2-base/healthcheck.sh
+++ packages/tf2-base/healthcheck.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-"${SERVER_DIR}/rcon" -H ${IP/0.0.0.0/127.0.0.1} -p ${PORT} -P ${RCON_PASSWORD} status
+status="$("${SERVER_DIR}/rcon" -H ${IP/0.0.0.0/127.0.0.1} -p ${PORT} -P ${RCON_PASSWORD} status)" || exit $?
+
+# extract hostname
+if [[ $status =~ hostname:[[:space:]](.+)[[:space:]]version[[:space:]]:[[:space:]] ]]; then
+  echo "hostname: ${BASH_REMATCH[1]}"
+fi
+
+# extract map
+if [[ $status =~ map[[:space:]]+:[[:space:]]([a-zA-Z0-9_]+) ]]; then
+  echo "map: ${BASH_REMATCH[1]}"
+fi
+
+# extract player count
+if [[ $status =~ players[[:space:]]:[[:space:]]([0-9]+)[[:space:]]humans,[[:space:]][0-9]+[[:space:]]bots[[:space:]]\(([0-9]+)[[:space:]]max\) ]]; then
+  echo "players: ${BASH_REMATCH[1]}/${BASH_REMATCH[2]}"
+fi
